93ef32ceeaa155269dc65d0d2d13f6cbd921636c
simonhn <simonhn@gmail.com>
2011-09-18 00:01:46 +1000

    bug fixes

--- app.rb
+++ app.rb
@@ -9,9 +9,8 @@ enable :sessions
 
 configure do
   #setup MySQL connection:  
-  
-  @config = YAML::load( File.open( 'config/settings.yml' ) )
-  @connection = "#{@config['adapter']}://#{@config['username']}:#{@config['password']}@#{@config['host']}/#{@config['database']}";
+  #@config = YAML::load( File.open( 'config/settings.yml' ) )
+  #@connection = "#{@config['adapter']}://#{@config['username']}:#{@config['password']}@#{@config['host']}/#{@config['database']}";
   #DataMapper.setup(:default, @connection)
   #DataMapper.finalize
   set :haml, {:format => :html5}
@@ -26,77 +25,115 @@ helpers do
       end
     end
 
-    def authorized?
-      @auth ||=  Rack::Auth::Basic::Request.new(request.env)
-      @user =  ENV['MY_SITE_USERNAME']
-      @pass = ENV['MY_SITE_SECRET']
-      @auth.provided? && @auth.basic? && @auth.credentials && @auth.credentials == [@user.to_s, @pass.to_s]
-    end
-    
-    
-    def search_spotify(q)
-      result = Hash.new
-      host_url = 'http://ws.spotify.com/search/1/track.json'
-      file = RestClient.get host_url, {:params => {:q => q}}        
-      json = Crack::JSON.parse(file)
-      json['tracks'].each do |j|
-        territories = j['album']['availability']['territories']
-        in_sweden = territories.include? 'SE'
-        worldwide = territories.include? 'worldwide'
-        spotify_uri = j['href']
-    	  if (in_sweden or worldwide) and !spotify_uri.empty?
-    	    result[spotify_uri] = j
-        end
-      end
+  def authorized?
+    @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+    @user =  ENV['MY_SITE_USERNAME']
+    @pass = ENV['MY_SITE_SECRET']
+    @auth.provided? && @auth.basic? && @auth.credentials && @auth.credentials == [@user.to_s, @pass.to_s]
+  end
       
-      return result
+  def search_spotify(q)
+    result = Hash.new
+    host_url = 'http://ws.spotify.com/search/1/track.json'
+    file = RestClient.get host_url, {:params => {:q => q}}        
+    json = Crack::JSON.parse(file)
+    json['tracks'].each do |j|
+      territories = j['album']['availability']['territories']
+      in_sweden = territories.include? 'SE'
+      worldwide = territories.include? 'worldwide'
+      spotify_uri = j['href']
+  	  if (in_sweden or worldwide) and !spotify_uri.empty?
+  	    result[spotify_uri] = j
+      end
     end
     
-    def add_to_playlist(track,spotify_playlist_id)
-      url = "http://localhost:1337/playlist/"+spotify_playlist_id+"/add?index=0"
-      uri = URI.escape(url)
-      result = RestClient.post uri, track.inspect
-      return result
-    end
+    return result
+  end
     
-    def show_playlist(spotify_playlist_id)
-      url = "http://localhost:1337/playlist/"+spotify_playlist_id
-      uri = URI.escape(url)
-      file = RestClient.get uri
-      json = Crack::JSON.parse(file)
-      return json
-    end
-    
-    def lookup_track(spotify_id)
-      host_url = 'http://ws.spotify.com/lookup/1/.json'
-      file = RestClient.get host_url, {:params => {:uri => spotify_id}}        
-      json = Crack::JSON.parse(file)
-      return json
-    end
+  def add_to_playlist(track,spotify_playlist_id,count=0)
+    url = "http://localhost:1337/playlist/#{spotify_playlist_id}/add?index=#{count.to_s}"
+    uri = URI.escape(url)
+    result = RestClient.post uri, track.inspect
+    return result
+  end
+  
+  def show_playlist(spotify_playlist_id)
+    url = "http://localhost:1337/playlist/"+spotify_playlist_id
+    uri = URI.escape(url)
+    file = RestClient.get uri
+    json = Crack::JSON.parse(file)
+    return json
+  end
+  
+  def playlist_count(spotify_playlist_id)
+    url = "http://localhost:1337/playlist/"+spotify_playlist_id
+    uri = URI.escape(url)
+    file = RestClient.get uri
+    json = Crack::JSON.parse(file)
+    return json['tracks'].length
+  end
+      
+  def lookup_track(spotify_id)
+    host_url = 'http://ws.spotify.com/lookup/1/.json'
+    file = RestClient.get host_url, {:params => {:uri => spotify_id}}        
+    json = Crack::JSON.parse(file)
+    return json
+  end
+  
+  def delete_track(spotify_playlist_id, index)
+    url = URI.escape("http://localhost:1337/playlist/#{spotify_playlist_id}/remove?index=#{index.to_s}&count=1")
+    result = RestClient.post url, :foo => 'bar'
+    return result
+  end
 end
 
 get '/' do
   @tracks = Hash.new
   @result = show_playlist('spotify:user:s%c3%a4ders%c3%a4rla:playlist:5Y55eeChjOVhzi58P0o0NZ')
+  i = 0
   @result['tracks'].each do |doc|
     result = lookup_track(doc)
-    @tracks[result['track']['name']] = result['track']['artists'].first['name']
+    @tracks[i] = result
+    i = i + 1
   end
   haml :index
 end
 
-post '/search' do
+get '/search' do
   @result = search_spotify(params[:q])
-  haml :search
+  puts @result.inspect
+  if @result.length == 0
+    flash[:error] = "No results"
+    redirect '/'
+  else
+    haml :search
+  end
+end
+
+get '/remove' do
+  index = params[:index]
+  if index
+    result = delete_track('spotify:user:s%c3%a4ders%c3%a4rla:playlist:5Y55eeChjOVhzi58P0o0NZ', index)
+    if result.code == 200
+      flash[:notice] = "Track has been removed"
+    else
+      flash[:error] = "Could not remove the track, try again"
+    end
+  end
+  redirect '/'
 end
 
 get '/add' do 
-  track_string = [ params[:track].to_s ]
-  result = add_to_playlist(track_string,'spotify:user:s%c3%a4ders%c3%a4rla:playlist:5Y55eeChjOVhzi58P0o0NZ')
-  if result.code == 200
-    flash[:notice] = "Your track has been added"
-  else
-    flash[:error] = "We could not add the track, try again"
+  playlist_id = 'spotify:user:s%c3%a4ders%c3%a4rla:playlist:5Y55eeChjOVhzi58P0o0NZ'
+  track_id = [ params[:track].to_s ]
+  if track_id
+    count = playlist_count(playlist_id)
+    result = add_to_playlist(track_id,playlist_id,count)
+    if result.code == 200
+      flash[:notice] = "Track has been added"
+    else
+      flash[:error] = "Could not add the track, try again"
+    end
   end
   redirect "/"
 end
---
+++ public/js/now-playing.js
@@ -0,0 +1,70 @@
+NowPlaying = function(api, user, interval) {
+    this.api = api;
+    this.user = user;
+    
+    /* AutoUpdate frequency - Last.fm API rate limits at 1/sec */
+    this.interval = interval || 5;
+};
+NowPlaying.prototype = {
+    
+    display: function(track)
+    {
+      //no change 
+       if ($('#artist').text() === track.artist && $('#track').text()===track.name )
+       {
+          //$('.now').removeClass("now");
+          //$('#playing').remove();
+          
+          $('#artist').text(track.artist);
+          $('.separator').text(" - ");
+          $('#track').text(track.name);
+           $('<span id="playing"><span><img alt="Now Playing" id="del-icon" src="/images/speaker.svg"></span></span>').hide().appendTo('div.track:contains('+track.name+')');
+          //$('div.track:contains('+track.name+')').addClass('now');
+          //var image = document.createElement('img');
+          //image.src = track.image;
+        }
+        else{
+          $('#playing').remove();
+          //$('.now').removeClass("now");
+          $('#artist').text(track.artist).hide().fadeIn();
+          $('.separator').text(" - ").hide().fadeIn();
+          $('#track').text(track.name).hide().fadeIn();
+          //$('div.track:contains('+track.name+')').append('<span id="playing"><img alt="Now Playing" id="del-icon" src="/images/speaker.svg"></span>').hide().fadeIn();
+           $('<span id="playing"><span><img alt="Now Playing" id="del-icon" src="/images/speaker.svg"></span></span>').hide().appendTo('div.track:contains('+track.name+')').fadeIn();
+          //$('div.track:contains('+track.name+')').addClass('now');
+        }
+    },
+    
+    update: function()
+    {
+        this.api.getNowPlayingTrack(
+            this.user,
+            jQuery.proxy(this.handleResponse, this), 
+            function(error) { console && console.log(error); }
+        );
+    },
+    
+    autoUpdate: function()
+    {
+        // Do an immediate update, don't wait an interval period
+        this.update();
+        
+        // Try and avoid repainting the screen when the track hasn't changed
+        setInterval(jQuery.proxy(this.update, this), this.interval * 1000);
+    },
+    
+    handleResponse: function(response)
+    {
+        if (response) {
+            this.display({
+                // The API response can vary depending on the user, so be defensive
+                artist: response.artist['#text'] || response.artist.name,
+                name: response.name,
+                image: response.image[0]['#text']
+            });
+        }
+        else {
+            this.display({artist: ' ', name: ''});
+        }
+    }
+};
